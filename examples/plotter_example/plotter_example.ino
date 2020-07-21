5eb86bfc704902a6cbb64bc6f6d2ceb741e2d90d
lady ada <limor@ladyada.net>
2020-07-21 15:48:38 -0400

    rename example and fix buffer overflow

--- examples/plotter_example/plotter_example.ino
+++ examples/plotter_example/plotter_example.ino
@@ -1,28 +1,40 @@
+/* This example will use the Arduino plotter to display a select number of wavelengths */
+
 #include <Adafruit_AS7341.h>
-#include <Wire.h>
+
 Adafruit_AS7341 as7341;
+
+
+
 void setup() {
-  // communication with the host computer serial monitor
   Serial.begin(115200);
+
+  // Wait for communication with the host computer serial monitor
   while (!Serial) {
     delay(1);
   }
+  
   if (!as7341.begin()){
     Serial.println("Could not find AS7341");
     while (1) { delay(10); }
   }
-  as7341.setLEDCurrent(0); // 40mA  
-  as7341.enableLED(true);
+
   as7341.setATIME(100);
   as7341.setASTEP(100);
   as7341.setGain(AS7341_GAIN_256X);
 }
+
+
 void loop() {
-  uint16_t readings[6];
+  uint16_t readings[12];
+
   if (!as7341.readAllChannels(readings)){
     Serial.println("Error reading all channels!");
     return;
   }
+
+  // These are 'out of order' so the colors match the default plotter
+  // drawing colors :)
   // #1 blue
   Serial.print(readings[2]); // F3 Blue
   Serial.print(", ");
