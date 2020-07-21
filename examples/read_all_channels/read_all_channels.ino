5c4051f768837cf275e9cbf0a8199841151f06a0
lady ada <limor@ladyada.net>
2020-07-21 15:45:46 -0400

    fix incorrect buffer size (was overflowing and corrupting values?) cleaner output and faster reads

--- examples/read_all_channels/read_all_channels.ino
+++ examples/read_all_channels/read_all_channels.ino
@@ -1,54 +1,65 @@
+/* This example will read all channels from the AS7341 and print out reported values */
 
 #include <Adafruit_AS7341.h>
-#include <Wire.h>
+
 Adafruit_AS7341 as7341;
+
+
 void setup() {
-  // communication with the host computer serial monitor
   Serial.begin(115200);
+
+  // Wait for communication with the host computer serial monitor
   while (!Serial) {
     delay(1);
   }
+  
   if (!as7341.begin()){
     Serial.println("Could not find AS7341");
     while (1) { delay(10); }
   }
+  
   as7341.setATIME(100);
   as7341.setASTEP(999);
   as7341.setGain(AS7341_GAIN_256X);
 }
 
 void loop() {
-  uint16_t readings[6];
-
+  uint16_t readings[12];
 
   if (!as7341.readAllChannels(readings)){
     Serial.println("Error reading all channels!");
-    // return;
+    return;
   }
-  Serial.print("ADC0/F1-");
+
+  Serial.print("ADC0/F1 415nm : ");
   Serial.println(readings[0]);
-  Serial.print("ADC1/F2-");
+  Serial.print("ADC1/F2 445nm : ");
   Serial.println(readings[1]);
-  Serial.print("ADC2/F3-");
+  Serial.print("ADC2/F3 480nm : ");
   Serial.println(readings[2]);
-  Serial.print("ADC3/F4-");
+  Serial.print("ADC3/F4 515nm : ");
   Serial.println(readings[3]);
+  Serial.print("ADC0/F5 555nm : ");
+
+  /* 
+  // we skip the first set of duplicate clear/NIR readings
   Serial.print("ADC4/Clear-");
   Serial.println(readings[4]);
   Serial.print("ADC5/NIR-");
   Serial.println(readings[5]);
-  Serial.print("ADC0/F5-");
+  */
+  
   Serial.println(readings[6]);
-  Serial.print("ADC1/F6-");
+  Serial.print("ADC1/F6 590nm : ");
   Serial.println(readings[7]);
-  Serial.print("ADC2/F7-");
+  Serial.print("ADC2/F7 630nm : ");
   Serial.println(readings[8]);
-  Serial.print("ADC3/F8-");
+  Serial.print("ADC3/F8 680nm : ");
   Serial.println(readings[9]);
-  Serial.print("ADC4/Clear-");
+  Serial.print("ADC4/Clear    : ");
   Serial.println(readings[10]);
-  Serial.print("ADC5/NIR-");
+  Serial.print("ADC5/NIR      : ");
   Serial.println(readings[11]);
 
-  delay(2000);
+  Serial.println();
 }
