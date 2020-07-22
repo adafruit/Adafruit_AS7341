40e7413d8277b2bd832b2fb3466d91af8736b41e
Doxygen CI <travis@travis-ci.org>
2020-07-22 00:30:09 +0000

    Deploy code docs to GitHub Pages Travis build:
    Commit:

--- html/search/all_6.js
+++ html/search/all_6.js
@@ -1,8 +1,11 @@
 var searchData=
 [
   ['getchannel',['getChannel',['../class_adafruit___a_s7341.html#afa8a730d24616f2c0f56026d4822855c',1,'Adafruit_AS7341']]],
-  ['gethighthreshold',['getHighThreshold',['../class_adafruit___a_s7341.html#acef03dd26727a4bcd6fa304a6f90947a',1,'Adafruit_AS7341']]],
+  ['getgpiodirection',['getGPIODirection',['../class_adafruit___a_s7341.html#a9db0123dcce6e1b2c02a9711767c3ec0',1,'Adafruit_AS7341']]],
+  ['getgpioinverted',['getGPIOInverted',['../class_adafruit___a_s7341.html#a5c2215216f86355194a94ab10d65c7ad',1,'Adafruit_AS7341']]],
+  ['getgpiovalue',['getGPIOValue',['../class_adafruit___a_s7341.html#a0d3c345f25aafedbb000061767918f23',1,'Adafruit_AS7341']]],
+  ['gethighthreshold',['getHighThreshold',['../class_adafruit___a_s7341.html#ac1c84cacb850eeefada38d7c41653136',1,'Adafruit_AS7341']]],
   ['getinterruptstatus',['getInterruptStatus',['../class_adafruit___a_s7341.html#a8d2d89787e090e382364c12ecc6da2eb',1,'Adafruit_AS7341']]],
   ['getisdataready',['getIsDataReady',['../class_adafruit___a_s7341.html#af3aa7dc42396b192a90c8f88c07b4111',1,'Adafruit_AS7341']]],
-  ['getlowthreshold',['getLowThreshold',['../class_adafruit___a_s7341.html#a206bfa47627361f2abdbe56e733cc576',1,'Adafruit_AS7341']]]
+  ['getlowthreshold',['getLowThreshold',['../class_adafruit___a_s7341.html#a3ef608890842cd5e13dcd921f75d41b5',1,'Adafruit_AS7341']]]
 ];
--- html/search/functions_5.js
+++ html/search/functions_5.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['enableled',['enableLED',['../class_adafruit___a_s7341.html#a911f919954c728fcd213922825888e51',1,'Adafruit_AS7341']]],
-  ['enablespectralint',['enableSpectralINT',['../class_adafruit___a_s7341.html#a4b8e99f37449358fa662c2d8076b230e',1,'Adafruit_AS7341']]],
-  ['enablespectralmeasurement',['enableSpectralMeasurement',['../class_adafruit___a_s7341.html#a545d170860b2934d776de73df4c8ab09',1,'Adafruit_AS7341']]]
+  ['enablespectralinterrupt',['enableSpectralInterrupt',['../class_adafruit___a_s7341.html#a75d1269a13b9fa53184e02c1517f23a4',1,'Adafruit_AS7341']]],
+  ['enablespectralmeasurement',['enableSpectralMeasurement',['../class_adafruit___a_s7341.html#a545d170860b2934d776de73df4c8ab09',1,'Adafruit_AS7341']]],
+  ['enablesysteminterrupt',['enableSystemInterrupt',['../class_adafruit___a_s7341.html#ab9e88fb7965fe8b9d23b7b0377dd52f9',1,'Adafruit_AS7341']]]
 ];
